0f929795ae4f28ee87a802fc6d76d53cc822e152
Alvaro Valdez <v.alvaro.r@gmail.com>
2025-09-29 02:49:06 -0300

    Conexion Mongo

--- src/config/mongo.js
+++ src/config/mongo.js
@@ -1,10 +1,23 @@
 // backend/src/config/mongo.js
 const mongoose = require('mongoose');
 
-async function connectMongo(uri) {
-  mongoose.set('strictQuery', true);
-  await mongoose.connect(uri);
-  console.log('[mongo] Conectado');
+async function connectMongo() {
+  const uri = process.env.MONGODB_URI;
+
+  if (!uri) {
+    throw new Error('[mongo] No está definida la variable MONGODB_URI');
+  }
+
+  try {
+    await mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('[mongo] Conectado a MongoDB');
+  } catch (err) {
+    console.error('[mongo] Error de conexión:', err);
+    process.exit(1);
+  }
 }
 
-module.exports = { connectMongo };
+module.exports = connectMongo;
