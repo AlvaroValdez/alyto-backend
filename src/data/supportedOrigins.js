e648bb829863d7da3fdfa138e5902996a9c25838
Alvaro Valdez <v.alvaro.r@gmail.com>
2025-12-01 23:16:05 -0300

    filtro desde admin para pais de origen

--- src/data/supportedOrigins.js
+++ src/data/supportedOrigins.js
@@ -1,9 +1,9 @@
-// Catálogo maestro de países soportados por la API de Vita Wallet para Pay-ins
+// Catálogo maestro de países soportados por la API para Pay-ins
 export const SUPPORTED_ORIGINS = [
     { code: 'CL', name: 'Chile', currency: 'CLP' },
     { code: 'CO', name: 'Colombia', currency: 'COP' },
     { code: 'AR', name: 'Argentina', currency: 'ARS' },
     { code: 'MX', name: 'México', currency: 'MXN' },
-    { code: 'BR', name: 'Brasil', currency: 'BRL' },
+    { code: 'BR', name: 'Brasil', currency: 'BRL' }, //PARA PIX EN TAKENOS ORIGEN ES DOLAR
     { code: 'PE', name: 'Perú', currency: 'PEN' },
 ];
