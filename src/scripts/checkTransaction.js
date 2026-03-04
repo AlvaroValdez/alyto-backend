40a023270d68b51296187e6f7c18f32bf01611d4
Alvaro Valdez <v.alvaro.r@gmail.com>
2026-03-04 19:13:42 -0300

    check transaction

---
+++ src/scripts/checkTransaction.js
@@ -0,0 +1,39 @@
+import mongoose from 'mongoose';
+import { env } from 'process';
+import dotenv from 'dotenv';
+dotenv.config();
+
+import Transaction from '../models/Transaction.js';
+import TransactionConfig from '../models/TransactionConfig.js';
+import Markup from '../models/Markup.js';
+
+async function run() {
+    try {
+        const mongoUri = process.env.MONGO_URI || 'mongodb+srv://app_user:vlow7LV14FLd0aNE@avf-vita.oh8gqvz.mongodb.net/?retryWrites=true&w=majority&appName=avf-vita';
+        await mongoose.connect(mongoUri);
+        console.log('Connected to DB');
+
+        const tx = await Transaction.findOne({ order: 'ORD-1772581258244' });
+        if (!tx) {
+            console.log('Transaction not found!');
+        } else {
+            console.log('--- Transaction ---');
+            console.log(JSON.stringify(tx, null, 2));
+        }
+
+        const config = await TransactionConfig.findOne({ originCountry: 'BO' });
+        console.log('--- BO Config ---');
+        console.log(JSON.stringify(config, null, 2));
+
+        const markup = await Markup.findOne({ originCountry: 'BO', destCountry: 'CO' });
+        console.log('--- BO -> CO Markup ---');
+        console.log(JSON.stringify(markup, null, 2));
+
+    } catch (err) {
+        console.error(err);
+    } finally {
+        mongoose.disconnect();
+    }
+}
+
+run();
